5ec308378dadb73cc048adc84675cfd7d01d3543
saugat dhakal <saugatdhakal5@gmail.com>
2022-06-05 22:35:24 +0545

    Login UI Completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,87 +1,93 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
-        
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+
 </head>
+
 <body>
-    <div class="container mt-5 ">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card ">
-                <center><div class="card-header"><h1>SARAL BILLING</h1></div></center>
-                
+    <div class="container" style="margin-top:5cm">
+        <div class="row">
+            <div class="card border border-3 shadow" style="height:700px">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <p style="text-align:center;font-size:35px;font-weight:700;margin-top:15px;color:rgb(0, 157, 255)">SARAL BILLING
+                            </p>
+                            <img src="{{ asset('assets/img/login.png') }}" width="750" style="margin-top:25px">
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div style="margin-top:100px">
+                                        <p style="font-size:27px;font-weight:700">USERNAME</p>
+                                        <div class="form-group row">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                                            <input id="email" type="email"
+                                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                                value="{{ old('email') }}" required autocomplete="email"
+                                                style="height:50px;font-size:25px;font-weight-bold" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        </div>
+                                    </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    <div class="">
+                                        <p style="font-size:27px;font-weight:700;margin-top:30px">PASSWORD</p>
+                                        <div class="form-group row mt-3">
+
+
+                                            <input id="password" type="password"
+                                                class="form-control @error('password') is-invalid @enderror"
+                                                name="password" required autocomplete="current-password"
+                                                style="height:50px;font-size:25px;font-weight-bold">
+
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mt-4">
+                                        <div class="col-md-6 "> {{-- offset-md-4 --}}
+                                            <div class="form-check"
+                                                style="font-size:20px;font-weight:700;margin-top:15px">
+                                                <input class="form-check-input" type="checkbox" name="remember"
+                                                    id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block"
+                                        style="margin-top:30px;height:50px;width:380px">LOG IN</button>
+
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group row mt-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mt-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0 mt-3">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 </body>
 
 </html>
-
